8b36ec83e1cbf4132dcc984798be63f4a25632f4
devDohyun <dohyun10101@gmail.com>
2021-03-25 23:05:31 +0900

    Added query params : page, id

--- routes/music/index.js
+++ routes/music/index.js
@@ -4,9 +4,17 @@ const router = express.Router()
 const modelMusic = require(__base + 'models/music')
 
 
-router.get('/', async (req, res) => {
-    const result = await modelMusic.getMusic()
+router.get('/:id?', async (req, res) => {
+    const page = req.query.page
+    const id = req.query.id
     
+    const params = {
+        page,
+        id
+    }
+    const result = await modelMusic.getMusic(params)
+    if (result.error) return res.send({ status: result.error })
+
     
     return res.send(result)
 })
